358b95bbe3a665a0f65b6b7a125210199888dc83
menoo20 <menooteaching@gmail.com>
2023-08-19 15:21:47 +0300

    preparing the last commit before adding to remote repo

--- src/server/getCityPic.js
+++ src/server/getCityPic.js
@@ -3,7 +3,6 @@ const axios = require("axios")
 
 const getCityPic = async(city, key) => {
     const {data} = await axios.get(`https://pixabay.com/api/?key=${key}&q=${city}&image_type=photo`)
-    console.log(data.hits[0]);
     const image =await data.hits[0]? await data.hits[0].webformatURL: "https://source.unsplash.com/random/640x480?city,morning,night?sig=1"
     if(image){
    // now i will send an object with single property image
